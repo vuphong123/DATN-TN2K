5aa708775e3ef8ba5738a0960b29f4b9f67b078a
vuphong123 <vutuanphong1782004@gmail.com>
2024-10-05 21:18:55 +0700

    rename

--- DATN-SD-18-Website-TN2K/src/main/java/com/poly/datn/sd10/DatnSd10WebsiteTn2KApplication.java
+++ DATN-SD-18-Website-TN2K/src/main/java/com/poly/datn/sd10/DatnSd10WebsiteTn2KApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DatnSd18WebsiteTn2KApplication {
+public class DatnSd10WebsiteTn2KApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DatnSd18WebsiteTn2KApplication.class, args);
+		SpringApplication.run(DatnSd10WebsiteTn2KApplication.class, args);
 	}
 
 }
